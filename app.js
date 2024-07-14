4d88d66d4008fa0aa597df2dc765471a5e9f1803
Daniel Ha <audanielha@gmail.com>
2024-07-14 23:55:57 +1200

    update js code

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-let friends = ["Dan", "John", "James", "Chris", "Todd"];
+let friends = ["Tim", "Tam", "Tom", "Tang"];
+
 
 for(let i = 0; i < friends.length; i++){
-    console.log(friends[i]+":")
-    for(let x = 99; x > 1; x--){
-    console.log(x + " lines of code in the file, "+ x + " lines of code; "+ friends[i] +" strikes one out, clears it all out,")
-        if(x == 2){
-            console.log("1 line of code in the file, 1 line of code; "+ friends[i] +" strikes one out, clears it all out, no more lines of code in the file")
+    console.log(`${friends[i]}:`)
+    for(let x = 99; x >= 1; x--){
+        if(x > 1){
+            console.log(`${x}lines of code in the file,`)
+        }
+        else{
+            console.log(`1 line of code in the file, 1 line of code; ${friends[i]} strikes one out, clears it all out, no more lines of code in the file`)
         }
     }
 }
+
+
+
